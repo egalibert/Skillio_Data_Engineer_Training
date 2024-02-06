94dcffce7b7f2f07bbe115bb50c3aff73c96bd5b
Elliot <elliotgalibert@gmail.com>
2024-02-06 09:28:19 +0200

    Day2 started

--- Week_1/Day1-2/Pandas_Exercises 2/Ex8.py
+++ Week_1/Day1-2/Pandas_Exercises 2/Ex8.py
@@ -1,6 +1,8 @@
 import pandas as pd
 
 df = pd.read_csv('titanic.csv')
-new_df = df.loc[(df['Siblings/Spouses Aboard'] > 0) & (df['Parents/Children Aboard'] > 0)].drop()
+
+# print(df)
+new_df = df[(df['Siblings/Spouses Aboard'] == 0) & (df['Parents/Children Aboard'] == 0)]
 
 print(new_df)
---
+++ Week_1/Day1-2/Pandas_Exercises 2/Ex9.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+df = pd.read_csv('titanic.csv')
+
+# print(df)
+new_df = df[(df['Siblings/Spouses Aboard'] > 0) & (df['Parents/Children Aboard'] > 0)]
+print(new_df['Fare'].mean())
