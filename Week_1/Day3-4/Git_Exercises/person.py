6d3a18d7ecde9f0ba75c3168aa1efbe60214ac54
Elliot <elliotgalibert@gmail.com>
2024-02-08 12:14:41 +0200

    two different files by accident

--- Week_1/Day3-4/Git_Exercises/person.py
+++ Week_1/Day3-4/Git_Exercises/person.py
@@ -1,32 +1,15 @@
 class Person:
-	def __init__(self, city, address, phone_number):
-		self._city = city
-		self._address = address
-		self._phone_number = phone_number
+	def __init__(self):
+		self.name = "Luffy"
+		self.age = 19
 
-	@property
-	def city(self, _default = None):
-		return self._city
-
-	@city.setter
-	def city(self, value):
-		self._city = value
-
-	@property
-	def address(self):
-		return self._address
-
-	@address.setter
-	def address(self, value):
-		self._address = value
-
-	@property
-	def phone_number(self, _default=None):
-		return self._phone_number
-
-	@phone_number.setter
-	def phone_number(self, value):
-		self._phone_number = value
-
-	def __str__(self):
-		return f'Address: {self._address}\nCity: {self._city}\nPhone: {self._phone_number}'
+	def get_name_and_age(self):
+		return (self.name, self.age)
+	
+	def set_name_and_age(self, name, age):
+		self.name = name
+		self.age = age
+		return (self.name, self.age)
+	
+	def __str__(self) -> str:
+		print(f"This person is called{self.name}, and is {self.age} years old")
