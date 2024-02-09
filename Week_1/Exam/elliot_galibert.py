87c0debccf2243b27b05bbeff5699701bf18fce8
Elliot <elliotgalibert@gmail.com>
2024-02-09 09:44:13 +0200

    Noticed small mistake, fixed

--- Week_1/Exam/elliot_galibert.py
+++ Week_1/Exam/elliot_galibert.py
@@ -5,12 +5,13 @@ df = pd.read_csv(file)
 cleaned_df = df[['Category', 'Material', 'Price']]
 cleaned_df = cleaned_df.dropna()
 
-average_price_df = cleaned_df.groupby(['Category', 'Material']).agg({'Price': 'mean'}).reset_index()
+final_df = cleaned_df.groupby(['Category', 'Material']).agg({'Price': 'mean'}).reset_index()
 
 # Rename the calculated column
-average_price_df.rename(columns={'Price': 'AveragePrice'}, inplace=True)
-average_price_df = average_price_df.drop(index=8)
-average_price_df = average_price_df.reset_index(drop=True)
+final_df.rename(columns={'Price': 'AveragePrice'}, inplace=True)
+final_df = final_df.drop(index=8)
+final_df = final_df.reset_index(drop=True)
+final_df['Category'] = final_df['Category'].str.replace(' ', '').str.replace('&', '')
 
 # print(cleaned_df)
-print(average_price_df)
+print(final_df)
